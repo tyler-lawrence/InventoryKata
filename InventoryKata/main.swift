c500e6fe352f21bd5ea017145406b2dfe6cad83f
Tyler Lawrence <tyler.lawrence15@gmail.com>
2026-03-02 08:39:03 -0500

    starting

--- InventoryKata/main.swift
+++ InventoryKata/main.swift
@@ -7,5 +7,74 @@
 
 import Foundation
 
-print("Hello, World!")
+enum ItemType {
+    case sword
+    case potion
+    case arrow
+
+    var isStackable: Bool {
+        switch self {
+        case .sword: return false
+        case .potion, .arrow: return true
+        }
+    }
+
+    var isConsumable: Bool {
+        switch self {
+        case .sword, .arrow: return false
+        case .potion: return true
+        }
+    }
+}
+
+struct ItemStack {
+    private(set) var itemType: ItemType
+    private(set) var quantity: Int
+
+    mutating func addItem(amount: Int = 1) {
+        guard itemType.isStackable else {
+            return
+        }
+        quantity += amount
+    }
+}
+
+extension Array where Element == ItemStack {
+    func indexFor(_ itemType: ItemType) -> Int? {
+        self.firstIndex(where: { $0.itemType == itemType })
+    }
+}
+
+struct Inventory {
+    private(set) var items: [ItemStack] = []
+
+    mutating func add(_ itemType: ItemType, quantity: Int = 1) {
+        if itemType.isStackable {
+            addItemToStack(itemType: itemType, quantity: quantity)
+        } else {
+            items.append(ItemStack(itemType: itemType, quantity: 1))
+        }
+    }
+
+    mutating func addItemToStack(itemType: ItemType, quantity: Int) {
+        if let stackIndex = items.indexFor(itemType) {
+            self.items[stackIndex].addItem()
+        } else {
+            items.append(ItemStack(itemType: itemType, quantity: quantity))
+        }
+    }
+
+    private func indexForItemStack(_ itemStack: ItemStack) -> Int? {
+        items.firstIndex(where: { $0.itemType == itemStack.itemType } )
+    }
+}
+
+struct Player {
+    let name: String
+    var inventory: Inventory = Inventory()
+
+    mutating func pickUpItem(_ itemType: ItemType) {
+        inventory.add(itemType)
+    }
+}
 
